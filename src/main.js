2fb06b7e27c3a9faa139b0fd263c8ba78fbb0bc4
Christine Campbell <ccamp67@gmail.com>
2020-05-30 15:41:50 -0700

    fixed the compiler

--- src/main.js
+++ src/main.js
@@ -12,8 +12,6 @@ Vue.use(VueLocalStorage, options);
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  render: h => h(App)
+}).$mount('#app')
